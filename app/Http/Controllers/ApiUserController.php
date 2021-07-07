6a3595dee4755e743c39241cc744ecd87fae2da2
minhvinhho <hominh4078@gmail.com>
2021-07-07 23:20:23 +0700

    register and login

---
+++ app/Http/Controllers/ApiUserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ApiRegisterRequest;
+use App\Http\Requests\ApiLoginRequest;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ApiUserController extends Controller
+{
+    public function register(ApiRegisterRequest $request){
+        $user = new User;
+        $user->fill($request->all());
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json($user);
+    }
+
+    public function login(ApiLoginRequest $request){
+        if(Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ]))
+        {
+            $user = User::whereEmail($request->email)->first();
+            $user->token = $user->createToken('App')->accessToken;
+            return response()->json($user);
+        }
+
+        abort('sai email hoac password', 403);
+    }    
+}
